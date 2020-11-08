1e35b5eedce3dbf2011c81cac64593269f79a2db
samnied <samuel.niederer@hsr.ch>
2020-11-08 13:45:23 +0100

    read also heap data from GDBlog

--- GDB-py-script.py
+++ GDB-py-script.py
@@ -33,6 +33,7 @@ gdb.execute("continue")
 
 data = "t" + gdb.execute("p *this.tBuff@this.tIndex", to_string=True)
 data += "s" + gdb.execute("p *this.sBuff@this.sIndex", to_string=True)
+data += "h" + gdb.execute("p *this.hBuff@this.hIndex", to_string=True)
 
 with open(r'log.txt', mode="w") as f:
     f.write(data)
