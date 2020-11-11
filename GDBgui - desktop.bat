37fecf04531e000f9f310ea9b69e2b752685705a
samnied <43746162+samnied@users.noreply.github.com>
2020-11-11 11:07:27 +0100

    remove unnecessary files

--- GDBgui - desktop.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-call C:\Users\sam\Anaconda3\Scripts\activate.bat
-C:\Users\sam\Anaconda3\python.exe "GDBgui.py"
--- GDBgui.bat
+++
@@ -1,3 +0,0 @@
-@echo on
-call C:\Users\samue\Anaconda3\Scripts\activate.bat
-C:\Users\samue\Anaconda3\python.exe "GDBgui.py"
